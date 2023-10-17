b906b26d972cd847ef84719241a4bab2f8cdee59
HyesonAnn <side111229@gmail.com>
2023-10-17 23:39:08 +0900

    test open api

--- src/main/kotlin/com/my/cubelog/dto/CubeResponseDto.kt
+++ src/main/kotlin/com/my/cubelog/dto/CubeResponseDto.kt
@@ -3,3 +3,33 @@ package com.my.cubelog.dto
 data class CubeResponseDto (
     val data: String = " "
 )
+
+data class OpenAPIRequestDto(
+    val count: Int = 1000,
+    val date: String = "2023-10-15",
+    val cursor: String? = null
+)
+
+data class CubeFromNexonResponseDto(
+    val count: Int,
+    val cube_histories: CubeHistoriesDto
+)
+
+data class CubeHistoriesDto(
+    val id: String,
+    val characte_name: String,
+    val cube_type: String,
+    val create_date: String,
+    val item_upgrade_result: String,
+    val miracle_time_flag: String,
+    val item_equip_part: String,
+    val item_level: Int,
+    val target_item: String,
+    val potential_option_grade: String,
+    val additional_potential_option_grade: String,
+    val upgrade_guarantee: Boolean,
+    val upgrade_guarantee_count: Int,
+    //val beforePotentialOptions: BeforePotentialOptionDto
+)
+
+//data class BeforePotentialOptionDto( )
--- src/main/kotlin/com/my/cubelog/service/CubeService.kt
+++ src/main/kotlin/com/my/cubelog/service/CubeService.kt
@@ -1,12 +1,33 @@
 package com.my.cubelog.service
 
 import com.my.cubelog.dto.CubeResponseDto
+import com.my.cubelog.dto.OpenAPIRequestDto
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
-class CubeService {
+class CubeService(private val webBuilder: WebClient.Builder, val httpService: HttpService){
     fun getCube(key: String): CubeResponseDto {
+        httpService.getCubeFromNexon(OpenAPIRequestDto(), key)
+        println(httpService.getCubeFromNexon(OpenAPIRequestDto(), key))
+
+        var a = CubeResponseDto()   //추후 가공데이터 입력
+        return a
+    }
+
+    fun getCubePersonalAverage(key: String): CubeResponseDto {
         var a = CubeResponseDto()
         return a
     }
+
+    fun getCubeAllAverage(key: String): CubeResponseDto {
+        var a = CubeResponseDto()
+        return a
+    }
+
+    fun getCubeSimulatorAverage(key: String): CubeResponseDto {
+        var a = CubeResponseDto()
+        return a
+    }
+
 }
